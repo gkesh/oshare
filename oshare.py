b2dbe53be80434ccddc243419aad425b7facdd88
gkesh <gkeshthapa@gmail.com>
2022-09-09 01:17:30 +0545

    Added styling support

--- oshare.py
+++ oshare.py
@@ -1,11 +1,16 @@
 from quart import Quart
+
 from home import bp_home
 from files import bp_files
 from text import bp_text
 from auth import bp_auth
 
 
-app = Quart(__name__)
+app = Quart(
+    __name__, 
+    static_folder="static",
+    static_url_path="/static"
+)
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 app.register_blueprint(bp_home)
