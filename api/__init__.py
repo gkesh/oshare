dbfcab0be1fa37e2f16db8dd70d9849763973f8e
gkesh <gkeshthapa@gmail.com>
2022-09-11 09:51:01 +0545

    Added version to api prefix and get greet request

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from quart import Blueprint
 from quart_schema import validate_request, validate_response
 from api.models import User, OshareResponse
@@ -6,11 +7,15 @@ from api.models import User, OshareResponse
 bp_api = Blueprint(
     "bp_api",
     __name__,
-    url_prefix="/api"
+    url_prefix="/api/v1"
 )
+
+@bp_api.get("/greet")
+async def greet_get() -> Tuple[OshareResponse, int]:
+    return OshareResponse("Hello there!"), 200
 
 @bp_api.post("/greet")
 @validate_request(User)
 @validate_response(OshareResponse, 200)
-async def greet(data: User) -> str:
+async def greet(data: User) -> Tuple[OshareResponse, int]:
     return OshareResponse(f"Hello there! {data.first_name} {data.last_name}."), 200
