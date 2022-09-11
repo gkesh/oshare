e46dfe7064d3c1a3bc1ff43b1e5d9d03497a947c
gkesh <gkeshthapa@gmail.com>
2022-09-11 21:35:46 +0545

    Added menu blur on outside click

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,11 +1,19 @@
 let menuActive = false;
 
 const menuTrigger = document.getElementById("menu-trigger");
+const menuBackground = document.getElementById("link-menu");
 
-menuTrigger?.addEventListener("mousedown", () => {
+menuTrigger?.addEventListener("mousedown", (event) => {
+    event.stopPropagation();
     const display = menuActive ? "none": "flex";
     const menu = document.getElementById("link-menu");
 
     menu.style.display = display;
     menuActive = !menuActive;
+});
+
+menuBackground?.addEventListener("mousedown", (event) => {
+    if (!event.target.classList.contains("menu")) return;
+    event.target.style.display = "none";
+    menuActive = false;
 });
